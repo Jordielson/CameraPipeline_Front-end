7389099cd7ef3dd4a15323c0fab1c64b3b29ffc8
Jordielson <jordielsonemanuel@gmail.com>
2022-09-14 10:33:43 -0300

    atuazacao no token

---
+++ d
@@ -0,0 +1,9 @@
+* [32mcrud-camera[m
+  dev[m
+  master[m
+  video-stream[m
+  [31mremotes/origin/HEAD[m -> origin/master
+  [31mremotes/origin/crud-camera[m
+  [31mremotes/origin/dev[m
+  [31mremotes/origin/master[m
+  [31mremotes/origin/video-stream[m
--- src/config/server/config.js
+++ src/config/server/config.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const Api = () => {
-    // const token = localStorage.getItem("token");
-    const token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqb2FvQGdtYWlsLmNvbSIsInNjb3BlcyI6IlJPTEVfVVNFUiIsImlhdCI6MTY2MjkyMzU0OSwiZXhwIjoxNjY0MTIzNTQ5fQ.NGoBjVtnURZx_P0t-istEJEHXDetbRZbrVYPqnaIeOU";
+    const token = localStorage.getItem("token");
     return axios.create({ 
         baseURL: "http://localhost:8080",
         headers: {
