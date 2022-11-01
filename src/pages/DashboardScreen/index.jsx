cc1e180440e079d8d143ba167f9b0b103d8e3b06
Victor Fernando <victor.macedo110697@gmail.com>
2022-11-01 13:54:10 -0300

    adicionado chart template

--- src/pages/DashboardScreen/index.jsx
+++ src/pages/DashboardScreen/index.jsx
@@ -1,4 +1,5 @@
 import SidebarMenu from "../../components/SideBarMenu";
+import Styles from "./Styles.module.css";
 
 function Dashboard() {
   return (
@@ -11,6 +12,16 @@ function Dashboard() {
               <a className="navbar-brand navbar-dark camera-title">Dashboard</a>
             </div>
           </nav>
+          <div className={Styles.containergrid}>
+            <div className={Styles.area2}> CHART</div>
+            <div className={Styles.area1}> CHART</div>
+            <div className={Styles.area3}>CHART </div>
+            <div className={Styles.area4}>CHART </div>
+            <div className={Styles.area5}>CHART </div>
+            <div className={Styles.area6}>CHART </div>
+            <div className={Styles.area7}>CHART </div>
+            <div className={Styles.area8}>CHART </div>
+          </div>
         </div>
       </div>
     </>
